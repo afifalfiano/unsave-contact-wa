792b8fa340981238eafe20a6d40b1a1186773851
Afif Alfiano <afif.alfiano@uii.ac.id>
2021-08-26 18:52:03 +0700

    fix: change from http request to href

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
-import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Component } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -9,28 +8,18 @@ import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms'
 })
 export class AppComponent {
   title = 'unsave-contact-wa';
+  correctValue: boolean | undefined;
   formWA: FormGroup = new FormGroup({
     phone: new FormControl('62', [Validators.required])
   });
   constructor(
-    private httpSvc: HttpClient
   ){}
 
   onSubmit(form: FormGroup): any {
-    console.log('Valid?', form.valid); // true or false
-    console.log('Name', form.value.phone);
     if (form.valid) {
-    const url = 'https://api.whatsapp.com/send';
-    const headerOption = {
-      headers: new HttpHeaders({
-        'Content-Type': 'application/json',
-        'Access-Control-Allow-Origin': '*',
-      })
-    };
-    const param = new HttpParams().set('phone', form.value.phone);
-    this.httpSvc.get(url, { params: param, headers: headerOption.headers }).subscribe((response) => {
-      console.log(response);
-    });
+    this.correctValue = true;
+    const url = 'https://api.whatsapp.com/send?phone=';
+    window.open(url + form.value.phone);
     }
   }
 }
